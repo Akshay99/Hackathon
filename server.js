a62317c5a17901bc74cb7d03e370d7fc30a8b215
Akshay <akshaypatil986@gmail.com>
2020-12-16 00:47:30 -0800

    done with all chanes

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ var express = require('express')
 var app = express()
 const mail = require('./src/mail');
 var https = require('https');
+var bodyParser = require('body-parser')
+const request = require('request');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/api/sendmail', function (req, res) {
   let htmlContent = `
-                <h1><strong>ITS Alerting</strong></h1>
                 <p>Dear User,</p>
-                <p>This is Test Mail</p>
-                <p> Please find your attachment for your document </p>
+                <p>Work Item No: XXXXXX Created Successfully</p>
+                <p>Please process the work item</p>
                 <br/>
                 `
   let mailOptions = {
     from: "akpatilvipl@gmail.com",
     to: "akpatilvipl@gmail.com",
-    subject: "Mail Test",
+    subject: "ITS Alert",
     text: "",
     html: htmlContent,
     //  attachment: ["/path"]
@@ -28,38 +34,26 @@ app.get('/api/sendmail', function (req, res) {
     });
 })
 
-app.post('/api/sendsms', function (req, response) {
-  //https://www.fast2sms.com/dev/bulk?authorization=gI8vVMt340xo1RkeADHzPNZYCKBabSOlwcfELm7ph9dsQFn6TGg0j9VxG6c7bCl5nTKqukvzUe2rDAs1&sender_id=FSTSMS&message=This is test message&language=english&route=p&numbers=9096101957
+app.post('/api/sendsms', function (req, res) {
+  const domain = 'https://www.fast2sms.com';
+  const auth = 'gI8vVMt340xo1RkeADHzPNZYCKBabSOlwcfELm7ph9dsQFn6TGg0j9VxG6c7bCl5nTKqukvzUe2rDAs1';
+  const message = 'Dear Customer, Transaction XXXXXX is processed successfully. please collect the document from the branch';
+  const fetchDatafromlocalDbUrl = 'http://localhost:3001/clients/' + req.body.customerId;
 
-  var options = {
-    host: 'fast2sms.com',
-    port: 443,
-    path: '/dev/bulk/',
-    method: 'GET',
-    query: {
-      authorization: 'gI8vVMt340xo1RkeADHzPNZYCKBabSOlwcfELm7ph9dsQFn6TGg0j9VxG6c7bCl5nTKqukvzUe2rDAs1',
-      sender_id: 'FSTSMS',
-      message: 'This is test SMS',
-      language: 'english',
-      route: 'p',
-      numbers: '9096101957'
-    }
-  };
-
-  var req = https.request(options, function (res) {
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      response.status(200).json({ success: true, msg: 'SMS sent Successfully' });
+  request(fetchDatafromlocalDbUrl, function (error, response, body) {
+    const contactNo = JSON.parse(body).phone;
+    console.log('Contact No ', contactNo);
+    const smsurl = `${domain}/dev/bulk?authorization=${auth}&sender_id=FSTSMS&message=${message}&language=english&route=p&numbers=${contactNo}`;
+    request(smsurl, function (error, response, body) {
+      console.error('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+      res.status(200).json({ success: true, msg: 'SMS sent Successfully' });
     });
   });
 
-  req.on('error', function (e) {
-    response.status(400).json({ success: false, msg: e });
-  });
 
-  req.write('data\n');
-  req.write('data\n');
-  req.end();
+
 })
 
 app.listen(3000, () => console.log(`Hello world app listening on port 3000!`))
