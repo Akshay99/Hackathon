13b4e3c61c521274846ca7a7e13aec94b0bd3593
Akshay <akshaypatil986@gmail.com>
2021-01-06 20:00:15 -0800

    modification

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+require('dotenv').config({ path: `${__dirname}/config/.env.local` });
+const path = require('path');
 const mail = require('./src/mail');
-var https = require('https');
-var bodyParser = require('body-parser')
+const https = require('https');
+const bodyParser = require('body-parser')
 const request = require('request');
+const { Console } = require('console');
+const { cookie } = require('request');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -35,20 +39,26 @@ app.get('/api/sendmail', function (req, res) {
 })
 
 app.post('/api/sendsms', function (req, res) {
-  const domain = 'https://www.fast2sms.com';
-  const auth = 'gI8vVMt340xo1RkeADHzPNZYCKBabSOlwcfELm7ph9dsQFn6TGg0j9VxG6c7bCl5nTKqukvzUe2rDAs1';
-  const message = 'Dear Customer, Transaction XXXXXX is processed successfully. please collect the document from the branch';
-  const fetchDatafromlocalDbUrl = 'http://localhost:3001/clients/' + req.body.customerId;
+  const fetchDatafromlocalDbUrl = `${process.env.dbapi}${req.body.customerId}`;
+  const smsDomain = process.env.smsdomain;
+  const auth = process.env.auth;
+  const message = process.env.message;
 
   request(fetchDatafromlocalDbUrl, function (error, response, body) {
     const contactNo = JSON.parse(body).phone;
-    console.log('Contact No ', contactNo);
-    const smsurl = `${domain}/dev/bulk?authorization=${auth}&sender_id=FSTSMS&message=${message}&language=english&route=p&numbers=${contactNo}`;
+    if (!contactNo) {
+      return res.status(400).json({ success: false, msg: 'No record or customer found' });
+    }
+    if (error) {
+      return res.status(400).json({ success: false, msg: 'Error occoured in sending Message' });
+    }
+    const smsurl = `${smsDomain}/dev/bulk?authorization=${auth}&sender_id=FSTSMS&message=${message}&language=english&route=p&numbers=${contactNo}`;
+
     request(smsurl, function (error, response, body) {
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-      res.status(200).json({ success: true, msg: 'SMS sent Successfully' });
+      if (error) {
+        return res.status(400).json({ success: false, msg: 'Error occoured in sending Message' });
+      }
+      res.status(200).json({ success: true, msg: 'SMS sent Successfully', info: JSON.parse(body).message });
     });
   });
 
